b13b90679dfc1037c7e84d9f4d4de24f6a00a0ab
Nikhil Dubbaka <info@technikh.com>
2023-08-18 16:13:33 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,6 @@ const Navbar = () => {
       <div className="container">
         <div className="navbar-brand">
           <Link to="/" className="navbar-item" title="Logo">
-           <!-- <img src={logo} alt="Pragament" style={{ width: "88px" }} /> -->
   <span class="site-name">Pragament</span>
           </Link>
           {/* Hamburger menu */}
@@ -39,38 +38,11 @@ const Navbar = () => {
                 About
               </Link>
             </li>
-          <!--  <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/products">
-              Products
-            </Link>
-            </li> -->
             <li className="navbar-item" style={{padding: "0px"}}>
             <Link className="navbar-item" to="/blog">
               Blog
             </Link>
             </li>
-            <!--  <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-            </li>
-          <li className="navbar-end has-text-centered">
-            <a
-              className="navbar-item"
-              href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
-          </li> -->
         </ul>
       </div>
     </nav>
